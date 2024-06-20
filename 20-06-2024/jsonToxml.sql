f8be3b4614a5505e051de3579b277d3b6edd46b5
Nithin1297 <nithin.madduri@proclink.com>
2024-06-20 18:17:02 +0530

    commiting on jun 20 with user defined function in sql

--- 20-06-2024/jsonToxml.sql
+++ 20-06-2024/jsonToxml.sql
@@ -93,85 +93,101 @@ for xml path
     from movies
     for xml path('Movie'), Root('Movies')
 
-select
-    movieId as [@movieId], -- attribute
-    movietitle as [MovieInfo/Title], -- nesting xml
-    [releaseYear] as [MovieInfo/Year],
-    directorID
-from movies
-for xml path('Movie'), Root('Movies')
+        select
+            movieId as [@movieId], -- attribute
+            movietitle as [MovieInfo/Title], -- nesting xml
+            [releaseYear] as [MovieInfo/Year],
+            directorID
+        from movies
+        for xml path('Movie'), Root('Movies')
 
-select *
-from movies
-for json auto
+            select *
+            from movies
+            for json auto
 
-select *
-from movies
-for json path,root('Movies')
+                select *
+                from movies
+                for json path,root('Movies')
 
-select
-    movieId as [Id], -- renaming the key
-    movietitle as 'MovieInfo.movietitle', -- nesting json
-    [releaseYear] as 'MovieInfo.releaseYear', -- nesting json
-    directorID
-from movies
-for json path, Root('Movies')
-
-
-
-select 
-		m.movieID as [@movieID], 
-		movietitle as [MovieInfo/movietitle], 
-		releaseYear as [MovieInfo/releaseYear], 
-		d.directorName, 
-		actorName
-FROM movies m
-    JOIN director d ON d.directorID = m.directorId
-    JOIN movieActors ma ON m.movieID = ma.movieID
-    JOIN actor a ON a.actorID = ma.actorID
-FOR XML Path ('Movie'), Root('Movies')
-
-
-select 
-		m.movieID as [ID], 
-		movietitle as 'MovieInfo.movietitle', 
-		[releaseYear] as 'MovieInfo.releaseYear', 
-		d.directorName, 
-		actorName
-FROM movies m
-    JOIN director d ON d.directorID = m.directorId
-    JOIN movieActors ma ON m.movieID = ma.movieID
-    JOIN actor a ON a.actorID = ma.actorID
-FOR JSON Path, Root('Movies')
+                    select
+                        movieId as [Id], -- renaming the key
+                        movietitle as 'MovieInfo.movietitle', -- nesting json
+                        [releaseYear] as 'MovieInfo.releaseYear', -- nesting json
+                        directorID
+                    from movies
+                    for json path, Root('Movies')
 
 
 
-select 
-		
-		movietitle as 'Movie.movietitle', 
-		[releaseYear] as 'Movie.releaseYear', 
-		m.movieID as 'Movie.MovieID', 
-		d.directorID as'Director.dierctorID',
-		d.directorName as'Director.dierctorName', 
-		a.actorID as 'Actor.actorID',
-		actorName as 'Actor.actorName'
-FROM movies m
-    JOIN director d ON d.directorID = m.directorId
-    JOIN movieActors ma ON m.movieID = ma.movieID
-    JOIN actor a ON a.actorID = ma.actorID
-FOR JSON Path, Root('Movies')
+                        select
+                            m.movieID as [@movieID],
+                            movietitle as [MovieInfo/movietitle],
+                            releaseYear as [MovieInfo/releaseYear],
+                            d.directorName,
+                            actorName
+                        FROM movies m
+                            JOIN director d ON d.directorID = m.directorId
+                            JOIN movieActors ma ON m.movieID = ma.movieID
+                            JOIN actor a ON a.actorID = ma.actorID
+                        FOR XML Path ('Movie'), Root('Movies')
 
-select 
-		movietitle as [Movie/movietitle], 
-		[releaseYear] as [Movie/releaseYear], 
-		m.movieID as [Movie/movieID], 
-		d.directorID as [Director/directorID],
-		d.directorName as[Director/directorName], 
-		a.actorID as [Actor/actorID],
-		actorName as [Actor/actorName]
-FROM movies m
-    JOIN director d ON d.directorID = m.directorId
-    JOIN movieActors ma ON m.movieID = ma.movieID
-    JOIN actor a ON a.actorID = ma.actorID
-FOR XML Path ('Movies'), Root('Root')
 
+                            select
+                                m.movieID as [ID],
+                                movietitle as 'MovieInfo.movietitle',
+                                [releaseYear] as 'MovieInfo.releaseYear',
+                                d.directorName,
+                                actorName
+                            FROM movies m
+                                JOIN director d ON d.directorID = m.directorId
+                                JOIN movieActors ma ON m.movieID = ma.movieID
+                                JOIN actor a ON a.actorID = ma.actorID
+                            FOR JSON Path, Root('Movies')
+
+
+
+                                select
+
+                                    movietitle as 'Movie.movietitle',
+                                    [releaseYear] as 'Movie.releaseYear',
+                                    m.movieID as 'Movie.MovieID',
+                                    d.directorID as'Director.dierctorID',
+                                    d.directorName as'Director.dierctorName',
+                                    a.actorID as 'Actor.actorID',
+                                    actorName as 'Actor.actorName'
+                                FROM movies m
+                                    JOIN director d ON d.directorID = m.directorId
+                                    JOIN movieActors ma ON m.movieID = ma.movieID
+                                    JOIN actor a ON a.actorID = ma.actorID
+                                FOR JSON Path, Root('Movies')
+
+                                    select
+                                        movietitle as [Movie/movietitle],
+                                        [releaseYear] as [Movie/releaseYear],
+                                        m.movieID as [Movie/movieID],
+                                        d.directorID as [Director/directorID],
+                                        d.directorName as[Director/directorName],
+                                        a.actorID as [Actor/actorID],
+                                        actorName as [Actor/actorName]
+                                    FROM movies m
+                                        JOIN director d ON d.directorID = m.directorId
+                                        JOIN movieActors ma ON m.movieID = ma.movieID
+                                        JOIN actor a ON a.actorID = ma.actorID
+                                    FOR XML Path ('Movies'), Root('Root')
+
+
+
+                                        select *
+                                        from movies
+
+                                        Drop function dbo.sayhiii
+
+                                        create Function sayhiii(@name varchar(20))
+Returns varchar(20)
+as 
+begin
+                                            return ('Hii ' + @name)
+                                        end;
+
+
+                                        select dbo.sayhiii('Nithin')
